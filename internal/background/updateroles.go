729e5fab6d18d36cfadb1f7161772618ffb21e44
Arturo Guerra <arturoguerra360@gmail.com>
2019-12-06 06:16:45 +0000

    Added member checking after initial join

--- internal/background/updateroles.go
+++ internal/background/updateroles.go
@@ -23,15 +23,17 @@ func filter(members []*discordgo.Member, rid string) []*discordgo.Member {
     for _, member := range members {
         createdAt, _ := discordgo.SnowflakeTimestamp(member.User.ID)
         loc, _ := time.LoadLocation("UTC")
-        now := time.Now().Loc(loc)
+        now := time.Now().In(loc)
         diff := now.Sub(createdAt)
 
-        days := int(dff.Hours()) / 24
+        days := int(diff.Hours()) / 24
 
-        if  (days >= 7 && checkroles(member.roles, rid)) {
+        if  (days >= 7 && checkroles(member.Roles, rid)) {
             mlist = append(mlist, member)
         }
     }
+
+    return mlist
 }
 
 func UpdateRoles(s *discordgo.Session) {
@@ -40,12 +42,12 @@ func UpdateRoles(s *discordgo.Session) {
     for true {
         guild, err := s.Guild("650109209610027034")
         if err == nil {
-            members := filter(guild.members, rid)
+            members := filter(guild.Members, rid)
             for _, member := range members {
-                checkmark.Member(s, m)
+                checkmark.Member(s, member)
             }
         }
 
-        time.Sleep( * time.Second)
+        time.Sleep(120 * time.Second)
     }
 }
