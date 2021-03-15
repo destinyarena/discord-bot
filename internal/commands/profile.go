9f176567eceb8f259b12ce3f43da197ed289eb49
Arturo Guerra <ar2roguerra@gmail.com>
2021-03-15 02:30:14 -0600

    Debuging

--- internal/commands/profile.go
+++ internal/commands/profile.go
@@ -17,11 +17,11 @@ type profile struct {
 func (c *profile) Init() {
 	c.Name = "profile"
 	c.Description = "Gets a members profile"
-	c.PermissionValidators = []func(*gommand.Context) (string, bool){
-		c.isOwner(),
-		c.isAdmin(),
-		c.isMod(),
-	}
+	//c.PermissionValidators = []func(*gommand.Context) (string, bool){
+	//	c.isOwner(),
+	//	c.isAdmin(),
+	//	c.isMod(),
+	//}
 }
 
 func getBannedValue(banned bool) string {
