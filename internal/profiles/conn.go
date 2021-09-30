e29f3bbfb4da4500ae582468432a6bc5ab233987
Arturo Guerra <ar2roguerra@gmail.com>
2021-09-30 13:20:42 -0500

    Debuging shit the right way

--- internal/profiles/conn.go
+++ internal/profiles/conn.go
@@ -6,13 +6,17 @@ import (
 )
 
 func (p *profiles) conn() (pb.ProfilesClient, *grpc.ClientConn, error) {
+	p.Logger.Info("GRPC Conn")
 	conn, err := grpc.Dial(p.URL, grpc.WithInsecure())
+	p.Logger.Info(conn)
+	p.Logger.Info(err)
 	if err != nil {
 		p.Logger.Error(err)
 		return nil, nil, err
 	}
 
 	client := pb.NewProfilesClient(conn)
+	p.Logger.Info(client)
 
 	return client, conn, nil
 
